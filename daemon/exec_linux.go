72b30eb9b0082e15e7dc32b3f13c606685aead48
jhowardmsft <jhoward@microsoft.com>
2015-04-24 11:12:56 -0700

    Windows: Split ContainerExecCreate
    Signed-off-by: John Howard <jhoward@microsoft.com>

---
+++ daemon/exec_linux.go
@@ -0,0 +1,17 @@
+// +build linux
+
+package daemon
+
+import (
+	"strings"
+
+	"github.com/docker/docker/daemon/execdriver/lxc"
+)
+
+// checkExecSupport returns an error if the exec driver does not support exec
+func checkExecSupport(drivername string) error {
+	if strings.HasPrefix(drivername, lxc.DriverName) {
+		return lxc.ErrExec
+	}
+	return nil
+}
---
+++ daemon/exec_windows.go
@@ -0,0 +1,8 @@
+// +build windows
+
+package daemon
+
+// checkExecSupport returns an error if the exec driver does not support exec
+func checkExecSupport(DriverName string) error {
+	return nil
+}
